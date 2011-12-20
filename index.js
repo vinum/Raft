94d1a37f2c25544dc6e1e2663222cb9196c1b336
Tim <admin@sharefarm.net>
2011-12-20 14:02:32 -0800

    init v0.0.001

---
+++ index.js
@@ -0,0 +1,26 @@
+/*!
+* Raft - utils
+* Copyright(c) 2010 ShareFarm.
+* MIT Licensed
+*/
+
+
+/**
+ * Module dependencies.
+ */
+
+var EventEmitter = require('events').EventEmitter
+
+/**
+ * Framework version.
+ */
+
+exports.version = '0.0.001';
+
+
+/**
+ * Expose utilities.
+ */
+
+exports.utils = utils;
+
