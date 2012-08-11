f00675ed39274fe947eb305c23943a0150d58d0a
Tim <timprice@mangoraft.com>
2012-08-10 23:28:18 -0700

    added log file reading

--- worker/logs.js
+++ worker/logs.js
@@ -10,14 +10,50 @@ var api = require('./server');
 
 var rootRoute = '/worker/logs'
 
+var LOG_LINEBREAK = "\n";
+var HISTORY_LENGTH = 100000;
 var logPaths = __dirname + '../logs'
 
 /**
  *
  */
+function error(req, res, next, err) {
+	res.json(500, {
+		pass : false,
+		err : {
+			message : err.message,
+			code : err.code || 'NO_CODE',
+			stack : err.stack
+		}
+	})
+}
+
 log.info('Raft-Worker-App', 'Route: ' + rootRoute + '/get/:name');
 api.get(rootRoute + '/get/:name', function(req, res, next) {
-	
+	var logName = req.param.name
+	var logFile = logPaths + '/' + logName;
+	var length = HISTORY_LENGTH;
+	var lines = [];
+	try {
+		var stat = fs.stats(logFile);
+		var fd = fs.openSync(logFile, 'r');
+		var text = fs.readSync(fd, length, Math.max(0, stat.size - length));
+		lines = text[0].split(LOG_LINEBREAK).reverse();
+		res.json(200, {
+			pass : true,
+			lines : lines
+		})
+	} catch(err) {
+		res.json(500, {
+			pass : false,
+			err : {
+				message : err.message,
+				code : err.code || 'NO_CODE',
+				stack : err.stack
+			}
+		})
+	}
+
 })
 /**
  *
