50c6f488ac27305f1bf3bea7f9e0f3039ae62031
flybyme <timprice@mangoraft.com>
2013-01-30 23:15:04 -0500

    update

--- raft/common/transports/socket.io.js
+++ raft/common/transports/socket.io.js
@@ -43,7 +43,7 @@ module.exports = function(service) {
 	// Create an `nssocket` TCP server
 	//
 	var io = require('socket.io').listen(raft.config.get('transports:socket.io:port'))
-	io.set("origins", "*:*|*");
+	io.set("origins", "*:*");
 	io.set('log level', 1);
 	io.set('transports', ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
 	io.sockets.on('connection', function(socket) {
@@ -56,8 +56,14 @@ module.exports = function(service) {
 				}
 			})
 		})
+		socket.on('error', function() {
+			//
+		})
 	});
 
+	io.sockets.on('error', function() {
+		//
+	})
 	raft.balancer.addApp({
 		domain : 'ws.api.' + raft.config.get('domain')
 	})
