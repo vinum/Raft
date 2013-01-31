96fe1ac31d0901f751a856fa4d0c16391538bdd9
flybyme <timprice@mangoraft.com>
2013-01-31 10:17:12 -0500

    fix

--- raft/service/user/logs/service.js
+++ raft/service/user/logs/service.js
@@ -33,7 +33,7 @@ exports.run = function(rpc) {
 		var user = this.user.username
 		var exposed = this
 
-		raft.drone.getlogs(uid, name, user, 100, function(err, data) {
+		raft.drone.getlogs(uid, name, user, 300, function(err, data) {
 			if (err) {
 				exposed.error(err)
 			} else {
