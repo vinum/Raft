a54f87df6a00ac98c7db90e8c1fedca35bc6fa34
fedora <git@mangoraft.com>
2014-04-24 19:48:54 -0400

    rewrite config loading now from the network

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  *
  */
 
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
+module.exports = new EventEmitter2()
 module.exports.common = require('./lib/raft/common')
 module.exports.start = require('./lib/raft/start')
-
 
